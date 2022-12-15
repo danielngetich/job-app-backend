89ddcb3e9855ec7b2781c5c76494f7db1b67233b
JerryJohnsJuma <mtotongwat254@gmail.com>
2022-12-15 17:02:20 +0300

    set up

--- app/controllers/job_listings_controller.rb
+++ app/controllers/job_listings_controller.rb
@@ -1,12 +1,34 @@
 class JobListingsController < ApplicationController
     def index
         job_listings=Job_listing.all
-        render json: job_listings
+        render json: job_listing, status: :ok
     end
     def create
         job_listing = Job_listing.create(job_listing_params)
         render json: job_listing, status: :created
     end
+    def update
+        job_listing = Job_listing.find_by(id:params[:id])
+          if job_listing
+                job_listing.update(job_listing_params)
+                render json: job_listing, status: :acepted
+            else
+                render json: {error: "Job_listing not found"}, status: :not_found
+             end
+     end
+
+    def destroy
+        job_listing = Job_listing.find_by(id:params[:id])
+          if job_listing
+                    job_listing.destroy
+                    head :no_content
+            else
+                render json: {error: "Job_listing not found"}, status: :not_found
+            end
+                             
+                
+      end
+            
 
     private 
     def job_listing_params
