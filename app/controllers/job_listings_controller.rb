cc25d9ee6309de2a231acad37835aca3ba0de89e
JerryJohnsJuma <mtotongwat254@gmail.com>
2022-12-15 16:30:11 +0300

    set up

--- app/controllers/job_listings_controller.rb
+++ app/controllers/job_listings_controller.rb
@@ -1,3 +1,16 @@
 class JobListingsController < ApplicationController
-    
+    def index
+        job_listings=Job_listing.all
+        render json: job_listings
+    end
+    def create
+        job_listing = Job_listing.create(job_listing_params)
+        render json: job_listing, status: :created
+    end
+
+    private 
+    def job_listing_params
+        params.permit(:job_title, :category, :description, :qualification, :deadline)
+    end
+
 end
