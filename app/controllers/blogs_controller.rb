b73899fbe0228f3475e584e1f6f55f079889c2a4
Christabel <christabel.masakhwe@student.moringaschool.com>
2022-12-16 13:06:50 +0300

    Add and update controllers

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,4 +3,12 @@ class BlogsController < ApplicationController
         blogs = Blog.all
         render json: blogs, status: :ok
     end
+    def show
+        blog = Blog.find_by(id:params[:id])
+        if blog
+              render json: blog, status: :ok
+          else
+              render json: {error: "Blog not found"}, status: :not_found
+           end
+    end
 end
--- app/controllers/job_listings_controller.rb
+++ app/controllers/job_listings_controller.rb
@@ -1,24 +1,32 @@
 class JobListingsController < ApplicationController
     def index
-        job_listings=Job_listing.all
-        render json: job_listing, status: :ok
+        job_listings = JobListing.all
+        render json: job_listings, status: :ok
     end
     def create
-        job_listing = Job_listing.create(job_listing_params)
+        job_listing = JobListing.create(job_listing_params)
         render json: job_listing, status: :created
     end
+    def show
+        job_listing = JobListing.find_by(id:params[:id])
+          if job_listing
+                render json: job_listing, status: :ok
+            else
+                render json: {error: "Job_listing not found"}, status: :not_found
+             end
+    end
     def update
-        job_listing = Job_listing.find_by(id:params[:id])
+        job_listing = JobListing.find_by(id:params[:id])
           if job_listing
                 job_listing.update(job_listing_params)
-                render json: job_listing, status: :acepted
+                render json: job_listing, status: :accepted
             else
                 render json: {error: "Job_listing not found"}, status: :not_found
              end
      end
 
     def destroy
-        job_listing = Job_listing.find_by(id:params[:id])
+        job_listing = JobListing.find_by(id:params[:id])
           if job_listing
                     job_listing.destroy
                     head :no_content
